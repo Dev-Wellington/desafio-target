4131feba1532762737d4e5d8a4e334bd8cae49b1
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2024-09-03 10:20:40 -0300

    Inverter uma String sem usar metodos prontos

---
+++ desafio_5.js
@@ -0,0 +1,11 @@
+// Desafio 5
+const string = 'Laranja';
+const strInvertida = [];
+
+for(let i = string.length-1;i >= 0;i--){
+    strInvertida.push(string[i])
+    
+}
+console.log(strInvertida)
+
+// resultado ajnaraL
