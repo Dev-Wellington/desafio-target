f724a910cd67240e4f1d81609762240e180d1b8d
Dev-Wellington <joycewellingtonaprigio2@gmail.com>
2024-09-03 10:17:44 -0300

    Desafio da soma

---
+++ desafio_1.js
@@ -0,0 +1,12 @@
+// Desafio 1
+let i = 13;
+let soma = 0;
+let k = 0;
+
+for(k = 0; k < i; k++){
+    soma = soma + k;
+}
+
+console.log('O valor da variavel soma é :',soma);
+
+// O valor da variavel soma é : 78
